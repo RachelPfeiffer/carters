926ab699fdbcfecb4606aafae590af440e3b4bce
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2019-01-29 12:44:03 +0200

    created gulpfile and top headers

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+browserSync.init({
+  server: 'app',
+  injectChanges: true
+});
+
+gulp.task('sass', function () {
+  return gulp.src('app/scss/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.stream({
+    match: '**/*.css'
+  }));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/scss/*.scss', gulp.series('sass'));
+  gulp.watch('app/*.html').on('change', browserSync.reload);
+});
